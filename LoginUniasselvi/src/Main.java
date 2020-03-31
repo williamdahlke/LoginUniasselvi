ad5fb61fb8a6399dc79156720a0ae64d8aa8337c
williamdahlke <williamgdahlke@gmail.com>
2020-03-30 21:29:46 -0300

    Inclusao das classes bases do app LoginUniasselvi

---
+++ LoginUniasselvi/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		PessoaFisica pessoa1 = new PessoaFisica();
+		pessoa1.setNome("William");
+		pessoa1.setCpf("12345678911");
+		System.out.println(pessoa1.getNome());
+
+	}
+
+}
---
+++ LoginUniasselvi/src/PessoaFisica.java
@@ -0,0 +1,18 @@
+public class PessoaFisica extends Pessoa {
+
+	private String cpf;
+	
+	public PessoaFisica() {
+		super();
+		this.cpf = "";
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+}
